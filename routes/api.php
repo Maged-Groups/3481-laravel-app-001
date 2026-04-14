ac78614116b785202372ed8db6d2716e880daae7
Maged Yaseen <magedyaseen@hotmail.com>
2026-04-14 20:53:20 +0200

    feat: cleanup

--- routes/api.php
+++ routes/api.php
@@ -1,30 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\{
-    TaskController,
-    EmployeeController,
-    ProductController
-};
-
-// Extra Routes
-Route::prefix('employees')->controller(EmployeeController::class)->group(function() {
-    Route::get('withdraw', 'withdraw');
-    Route::get('candidate', 'candidate');
-    Route::get('new', 'new');
-    Route::get('training', 'training');
-    Route::get('vacations', 'vacations');
-    Route::get('day-off', 'dayOff');
-    Route::get('permissions/{type}', 'permissions');
-});
-
-// API Routes
-Route::apiResources([
-    'tasks' => TaskController::class,
-    'employees' => EmployeeController::class,
-    'products' => ProductController::class,
-]);
-
-Route::fallback(function () {
-    return view('page-404');
-});
