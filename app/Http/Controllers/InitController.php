f72a07b351f8349b4643efc8ee45e1e221776d38
Maged Yaseen <magedyaseen@hotmail.com>
2026-04-18 22:56:18 +0200

    feat: add models init method to create all models at once

--- app/Http/Controllers/InitController.php
+++ app/Http/Controllers/InitController.php
@@ -6,6 +6,15 @@ use Illuminate\Support\Facades\Artisan;
 
 class InitController extends Controller
 {
+    private $models = [
+        'PostStatus',
+        'ReactionType',
+        'Post',
+        'Comment',
+        'Reply',
+        'Reaction',
+    ];
+
     public function migrations()
     {
 
@@ -18,13 +27,38 @@ class InitController extends Controller
             'reactions',
         ];
 
-        foreach($tables as $table) {
+        foreach ($tables as $table) {
             Artisan::call('make:migration', [
-                'name' => "create_$table"
+                'name' => "create_$table",
             ]);
-            
+
             sleep(1);
         }
 
+    }
+
+    public function controllers()
+    {
+
+        foreach ($this->models as $model) {
+            Artisan::call('make:controller', [
+                'name' => $model.'Controller',
+                // '-r' => true
+                '--api' => true,
+            ]);
+        }
+    }
+
+    public function Models()
+    {
+
+        foreach ($this->models as $model) {
+            Artisan::call('make:model', [
+                'name' => $model,
+                '-a' => true,
+            ]);
+
+            sleep(1);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,6 @@ use Illuminate\Support\Facades\Route;
 // Init
 Route::prefix('init')->controller(InitController::class)->group(function () {
     Route::get('migrations', 'migrations');
+    Route::get('controllers', 'controllers');
+    Route::get('models', 'models');
 });
