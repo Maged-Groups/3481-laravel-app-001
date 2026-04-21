e70128e289a65a764e846ef502714679b71d5390
Maged Yaseen <magedyaseen@hotmail.com>
2026-04-21 21:33:28 +0200

    feat: prepare all routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,15 @@
 
 use App\Http\Controllers\InitController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{
+    CommentController,
+    PostController,
+    PostStatusController,
+    ReactionController,
+    ReactionTypeController,
+    ReplyController,
+    UserController,
+};
 
 // Init
 Route::prefix('init')->controller(InitController::class)->group(function () {
@@ -9,3 +18,13 @@ Route::prefix('init')->controller(InitController::class)->group(function () {
     Route::get('controllers', 'controllers');
     Route::get('models', 'models');
 });
+
+Route::apiResources([
+    'comments' => CommentController::class,
+    'posts' => PostController::class,
+    'posts-statuses' => PostStatusController::class,
+    'reactions' => ReactionController::class,
+    'reaction-types' => ReactionTypeController::class,
+    'replies' => ReplyController::class,
+    'users' => UserController::class,
+]);
